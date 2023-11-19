565fe0522e055f5dee66edd3c7230922f48bdb1f
KaraitPL <karaitpl@gmail.com>
2023-11-20 00:37:56 +0100

    Implement window dragging functionality

--- SanityWidget/MainWindow.xaml.cs
+++ SanityWidget/MainWindow.xaml.cs
@@ -20,6 +20,9 @@ namespace SanityWidget
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private bool isDragging = false;
+        private Point offset;
         public MainWindow()
         {
             InitializeComponent();
@@ -29,6 +32,27 @@ namespace SanityWidget
         {
             Left = SystemParameters.WorkArea.Right - Width;
             Top = 80;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            isDragging = true;
+            offset = e.GetPosition(this);
+        }
+
+        private void Window_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (isDragging)
+            {
+                Point currentMousePosition = e.GetPosition(this);
+                this.Left = currentMousePosition.X - offset.X + this.Left;
+                this.Top = currentMousePosition.Y - offset.Y + this.Top;
+            }
+        }
+
+        private void Window_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            isDragging = false;
         }
     }
 }
